e38745c2feca36378b0602f498c3e1fbfd09b7be
choi-woo-seong <goqus903@naver.com>
2025-04-21 14:51:20 +0900

    test

---
+++ src/main/java/com/project/service/TestService.java
@@ -0,0 +1,10 @@
+package com.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
